2cbfb941d966535fab3c0a9542bc097a9f8c6304
harshit136 <83019055+harshit136@users.noreply.github.com>
2022-12-18 14:52:48 +0530

    login page vi

--- src/main/java/com/example/supplychainmanagementsystembyharshit/HelloApplication.java
+++ src/main/java/com/example/supplychainmanagementsystembyharshit/HelloApplication.java
@@ -3,6 +3,10 @@ package com.example.supplychainmanagementsystembyharshit;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -10,8 +14,27 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
 
+    private GridPane loginPage(){
+        Label emailLabel=new Label("Email");
+        Label passwordLabel=new Label("Password");
+
+        TextField emailTextField=new TextField();
+        PasswordField passwordField=new PasswordField();
+
+        GridPane gridPane=new GridPane();
+// first is x,second is y
+        gridPane.add(emailLabel,0,0);
+        gridPane.add(emailTextField,1,0);
+        gridPane.add(passwordLabel,0,1);
+        gridPane.add(passwordField,1,1);
+
+        return gridPane;
+    }
+
     private Pane createContent(){
        Pane root=new Pane();
+
+       root.getChildren().addAll(loginPage());
        return root;
     }
 
